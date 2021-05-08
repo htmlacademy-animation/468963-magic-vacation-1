2441f53b9e5e5c75cfa6d7ceb9244e5641b5d6d4
voice200 <voice200rabota@gmail.com>
2021-05-08 22:19:59 +0200

    added animation for menu and social icon

--- source/js/modules/social.js
+++ source/js/modules/social.js
@@ -2,8 +2,23 @@ export default () => {
   let socialBlock = document.querySelector(`.js-social-block`);
   socialBlock.addEventListener(`mouseover`, function () {
     socialBlock.classList.add(`social-block--active`);
+    document.querySelector(`social-block__list`).classList.remove(`display-none`);
+    document.querySelectorAll(`.social-block__link`).forEach((item) => {
+      item.classList.add(`active-link`);
+    });
   });
   socialBlock.addEventListener(`mouseleave`, function () {
     socialBlock.classList.remove(`social-block--active`);
+    document.querySelectorAll(`.social-block__link`).forEach((item) => {
+      item.classList.remove(`active-link`);
+    });
+    document.querySelector(`social-block__list`).classList.add(`display-none`);
+  });
+  socialBlock.addEventListener(`click`, ()=>{
+    socialBlock.classList.add(`social-block--active`);
+    document.querySelectorAll(`.social-block__link`).forEach((item) => {
+      item.classList.add(`active-link`);
+    });
+    document.querySelector(`social-block__list`).classList.remove(`display-none`);
   });
 };
