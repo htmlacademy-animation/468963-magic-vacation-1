aea524e2cd09a22b39dec39a898873b3fd90f596
voice200 <voice200rabota@gmail.com>
2021-05-13 22:45:47 +0200

    added animation for menu and social icon

--- source/js/modules/social.js
+++ source/js/modules/social.js
@@ -2,23 +2,26 @@ export default () => {
   let socialBlock = document.querySelector(`.js-social-block`);
   socialBlock.addEventListener(`mouseover`, function () {
     socialBlock.classList.add(`social-block--active`);
-    document.querySelector(`social-block__list`).classList.remove(`display-none`);
+    document.querySelector(`.social-block__list`).classList.remove(`display-none`);
     document.querySelectorAll(`.social-block__link`).forEach((item) => {
       item.classList.add(`active-link`);
+      item.classList.remove(`transition-delay`);
     });
   });
-  socialBlock.addEventListener(`mouseleave`, function () {
+  socialBlock.addEventListener(`mouseout`, function () {
     socialBlock.classList.remove(`social-block--active`);
     document.querySelectorAll(`.social-block__link`).forEach((item) => {
       item.classList.remove(`active-link`);
+      item.classList.add(`transition-delay`);
     });
-    document.querySelector(`social-block__list`).classList.add(`display-none`);
+    document.querySelector(`.social-block__list`).classList.add(`display-none`);
   });
   socialBlock.addEventListener(`click`, ()=>{
     socialBlock.classList.add(`social-block--active`);
     document.querySelectorAll(`.social-block__link`).forEach((item) => {
       item.classList.add(`active-link`);
+      item.classList.remove(`transition-delay`);
     });
-    document.querySelector(`social-block__list`).classList.remove(`display-none`);
+    document.querySelector(`.social-block__list`).classList.remove(`display-none`);
   });
 };
--- source/js/script.js
+++ source/js/script.js
@@ -8,6 +8,7 @@ import result from './modules/result.js';
 import form from './modules/form.js';
 import social from './modules/social.js';
 import FullPageScroll from './modules/full-page-scroll';
+import animation from './animation';
 
 // init modules
 mobileHeight();
@@ -18,6 +19,16 @@ chat();
 result();
 form();
 social();
+
+const ready = () =>{
+  if (!document.querySelector(`section`).classList.contains(`screen--hidden`)) {
+    animation();
+    return;
+  } else {
+    ready();
+  }
+};
+ready();
 
 const fullPageScroll = new FullPageScroll();
 fullPageScroll.init();
