452f023e91f01baf4f7e2bb569854f13485ed3f1
voice200 <voice200rabota@gmail.com>
2021-06-16 23:50:13 +0200

    added animation for logo, footer, header, social

--- source/js/animation.js
+++ source/js/animation.js
@@ -1,5 +1,15 @@
 export default () => {
   setTimeout(()=>{
     document.querySelector(`.intro__message`).classList.add(`active`);
-  }, 500);
+  }, 1000);
+  window.addEventListener(`load`, ()=>{
+    const menu = document.querySelector(`#menu`);
+    const footer = document.querySelector(`.screen__footer`);
+    const label = document.querySelector(`.page-header__logo`);
+    const social = document.querySelector(`.page-header__social`);
+    menu.classList.remove(`page-header__menu_not-active`);
+    footer.classList.remove(`screen__footer_not-active`);
+    social.classList.remove(`page-header__social_not-active`);
+    label.classList.remove(`page-header__logo_not-active`);
+  });
 };
