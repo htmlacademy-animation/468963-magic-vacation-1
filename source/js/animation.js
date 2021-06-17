e595bdaab653afa4f71a8d27fe522ef5ff8e5e1a
voice200 <voice200rabota@gmail.com>
2021-06-18 00:16:32 +0200

    fixed remarks

--- source/js/animation.js
+++ source/js/animation.js
@@ -7,10 +7,11 @@ export default () => {
     const menu = document.querySelector(`#menu`);
     const footer = document.querySelector(`.screen__footer`);
     const label = document.querySelector(`.page-header__logo`);
-    const social = document.querySelector(`.page-header__social`);
-    menu.classList.remove(`page-header__menu_not-active`);
-    footer.classList.remove(`screen__footer_not-active`);
-    social.classList.remove(`page-header__social_not-active`);
-    label.classList.remove(`page-header__logo_not-active`);
+    const social = document.querySelector(`.social-block__toggler`);
+    console.log('social', social);
+    menu.classList.add(`page-header__menu_active`);
+    footer.classList.add(`screen__footer_active`);
+    social.classList.add(`active-social`);
+    label.classList.add(`page-header__logo_active`);
   });
 };
