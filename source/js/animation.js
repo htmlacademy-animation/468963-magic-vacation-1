90ad9a08c6753a4e97291581434f2454ee508e9e
voice200 <voice200rabota@gmail.com>
2021-11-27 23:09:55 +0200

    competed task

--- source/js/animation.js
+++ source/js/animation.js
@@ -8,7 +8,6 @@ export default () => {
     const footer = document.querySelector(`.screen__footer`);
     const label = document.querySelector(`.page-header__logo`);
     const social = document.querySelector(`.social-block__toggler`);
-    console.log('social', social);
     menu.classList.add(`page-header__menu_active`);
     footer.classList.add(`screen__footer_active`);
     social.classList.add(`active-social`);
--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -4,6 +4,11 @@ export default () => {
   let menuLinks = document.querySelectorAll(`.js-menu-link`);
   const linkGame = document.querySelector(`#linkGame`);
   const messageField = document.getElementById(`message-field`);
+  const linkPizes = document.getElementById(`prizes-link`);
+  const blockAnimationFill = document.querySelector(`.animation-fill-screen`);
+  const blockPrize = document.getElementById(`prizes`);
+  const blockStory = document.getElementById(`story`);
+  const classActive = `active`;
 
   if (menuToggler) {
     menuToggler.addEventListener(`click`, function () {
@@ -21,14 +26,35 @@ export default () => {
     menuLinks[i].addEventListener(`click`, function () {
       if (menuLinks[i] === linkGame) {
         setTimeout(()=>{
-          messageField.classList.add(`active`);
+          messageField.classList.add(classActive);
         }, 100);
       } else {
-        messageField.classList.remove(`active`);
+        messageField.classList.remove(classActive);
       }
       if (window.innerWidth < 1025) {
         header.classList.remove(`page-header--menu-opened`);
         document.body.classList.remove(`menu-opened`);
+      }
+      if (menuLinks[i] === linkPizes) {
+        if (blockStory.classList.contains(classActive)) {
+          blockAnimationFill.classList.add(classActive);
+          setTimeout(() => {
+            blockAnimationFill.classList.remove(classActive);
+            blockStory.classList.remove(classActive);
+            blockStory.classList.add(`screen--hidden`);
+            blockPrize.classList.remove(`screen--hidden`);
+            blockPrize.classList.add(classActive);
+          }, 700);
+        } else {
+          blockPrize.classList.remove(`screen--hidden`);
+          blockPrize.classList.add(classActive);
+        }
+        menuLinks[i].classList.add(classActive);
+        menuLinks.forEach((link) => {
+          if (link !== linkPizes) {
+            link.classList.remove(classActive);
+          }
+        });
       }
     });
   }
